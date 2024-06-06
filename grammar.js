0ad62cb635c2f4353359a88dec9e3a57bbf9f66d
Uy Ha <hchanuy@gmail.com>
2024-06-06 07:46:11 +0000

    add parsing comments that start with `;`

--- grammar.js
+++ grammar.js
@@ -1,12 +1,14 @@
 module.exports = grammar({
   name: "eds",
   rules: {
-    source_file: ($) => repeat($.section),
+    source_file: ($) => repeat(choice($.section, $.comment)),
     section: ($) => seq("[", $.section_name, "]", repeat($.statement)),
     section_name: ($) => /[^\r\n\[\]]+/,
 
     key: ($) => /[^\r\n\[\]=]+/,
     value: ($) => /[^\r\n\[\]]+/,
     statement: ($) => seq($.key, "=", $.value),
+
+    comment: ($) => /;.*/,
   },
 });
